95896b8bcd65ba30e5d3cae02f37bd513b65f49a
Chris Down <chris@chrisdown.name>
2018-08-19 17:34:23 -0400

    Add basic tests for first fixer
    See #3. Hopefully eventually we'll be able to migrate this to
    quickcheck, but for now codify expectations prior to the nom migration
    for #14.

--- src/fixers.rs
+++ src/fixers.rs
@@ -1,10 +1,11 @@
 use regex::{Regex, RegexBuilder};
 use taglib;
 use types::{Fixer, MackError, Track};
+use std::borrow::Cow;
 
 lazy_static! {
     static ref FEAT_RE: Regex = RegexBuilder::new(
-        r#" [(\[]?feat[^.]*\. (?P<artists>[^)]+)[)\]]?"#
+        r#" [(\[]?f(ea)?t[a-z]*\.? (?P<artists>[^)]+)[)\]]?"#
     ).case_insensitive(true).build().unwrap();
 }
 
@@ -25,10 +26,31 @@ pub fn run_fixers(track: &mut Track, dry_run: bool) -> Result<Vec<Fixer>, MackEr
 
 fn fix_feat(tags: &mut taglib::Tag) -> Result<Option<Fixer>, MackError> {
     let old_title = tags.title();
-    let new_title = FEAT_RE.replace_all(&old_title, " (feat. $artists)");
+    let new_title = normalise_feat(&old_title);
     if old_title != new_title {
         tags.set_title(&new_title);
         return Ok(Some(Fixer::FEAT));
     }
     Ok(None)
+}
+
+fn normalise_feat<'a>(input: &'a str) -> Cow<'a, str> {
+    FEAT_RE.replace_all(input, " (feat. $artists)")
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_normalise_feat() {
+        let exp = "Foo Bar (feat. Baz and Quux)";
+        assert_eq!(normalise_feat(exp), exp);
+        assert_eq!(normalise_feat("Foo Bar feat. Baz and Quux"), exp);
+        assert_eq!(normalise_feat("Foo Bar feat Baz and Quux"), exp);
+        assert_eq!(normalise_feat("Foo Bar ft. Baz and Quux"), exp);
+        assert_eq!(normalise_feat("Foo Bar featuring Baz and Quux"), exp);
+        assert_eq!(normalise_feat("Foo Bar Feat Baz and Quux"), exp);
+    }
 }
