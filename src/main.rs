8fdb59326a66e163d85950c1e7de1fe5495e8fe3
Chris Down <chris@chrisdown.name>
2018-08-18 21:12:41 -0400

    Add directory walker

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+extern crate ignore;
+extern crate id3;
+extern crate clap;
+
+use ignore::Walk;
+use clap::{App, Arg};
+
 fn main() {
-    println!("mack");
+    let args = App::new("mack")
+        .about("The opinionated music library organiser.")
+        .arg(Arg::with_name("DIR").index(1))
+        .get_matches();
+
+    for result in Walk::new(args.value_of("DIR").unwrap_or(".")) {
+        match result {
+            Ok(entry) => println!("{}", entry.path().display()),
+            Err(err) => println!("ERROR: {}", err),
+        }
+    }
 }
