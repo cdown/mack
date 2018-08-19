ec89f0868254a0ec5393143f96bd5c79debb66a1
Chris Down <chris@chrisdown.name>
2018-08-18 21:36:23 -0400

    Filter on mp3 files

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,19 @@
-extern crate ignore;
-extern crate id3;
 extern crate clap;
+extern crate failure;
+extern crate id3;
+extern crate ignore;
 
-use ignore::Walk;
 use clap::{App, Arg};
+//use failure::{err_msg, Error};
+use ignore::WalkBuilder;
+use ignore::types::{Types, TypesBuilder};
+
+fn get_supported_music_types() -> Result<Types, ignore::Error> {
+    let mut ig_types = TypesBuilder::new();
+    ig_types.add("music", "*.mp3")?;
+    ig_types.select("music");
+    ig_types.build()
+}
 
 fn main() {
     let args = App::new("mack")
@@ -11,10 +21,17 @@ fn main() {
         .arg(Arg::with_name("DIR").index(1))
         .get_matches();
 
-    for result in Walk::new(args.value_of("DIR").unwrap_or(".")) {
+    let music_types = get_supported_music_types().expect("Failed to build music type map");
+    let walker = WalkBuilder::new(args.value_of("DIR").unwrap_or(".")).types(music_types).build();
+
+    for result in walker {
         match result {
-            Ok(entry) => println!("{}", entry.path().display()),
-            Err(err) => println!("ERROR: {}", err),
+            Ok(entry) => {
+                if entry.path().is_file() {
+                    println!("{}", entry.path().display());
+                }
+            }
+            Err(err) => eprintln!("error: {}", err),
         }
     }
 }
