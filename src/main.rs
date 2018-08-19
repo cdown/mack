48963926f9f061e3424e41c5290f2b7da771380e
Chris Down <chris@chrisdown.name>
2018-08-18 21:43:55 -0400

    Extract walker building into separate function

--- src/main.rs
+++ src/main.rs
@@ -3,27 +3,23 @@ extern crate failure;
 extern crate id3;
 extern crate ignore;
 
-use clap::{App, Arg};
-//use failure::{err_msg, Error};
-use ignore::WalkBuilder;
-use ignore::types::{Types, TypesBuilder};
-
-fn get_supported_music_types() -> Result<Types, ignore::Error> {
-    let mut ig_types = TypesBuilder::new();
-    ig_types.add("music", "*.mp3")?;
-    ig_types.select("music");
-    ig_types.build()
+fn build_music_walker(dir: &str) -> Result<ignore::Walk, ignore::Error> {
+    let mut mt_builder = ignore::types::TypesBuilder::new();
+    mt_builder.add("music", "*.mp3")?;
+    mt_builder.select("music");
+    let music_types = mt_builder.build()?;
+    Ok(ignore::WalkBuilder::new(dir).types(music_types).build())
 }
 
 fn main() {
-    let args = App::new("mack")
+    let args = clap::App::new("mack")
         .about("The opinionated music library organiser.")
-        .arg(Arg::with_name("DIR").index(1))
+        .arg(clap::Arg::with_name("DIR").index(1))
         .get_matches();
 
-    let music_types = get_supported_music_types().expect("Failed to build music type map");
-    let walker = WalkBuilder::new(args.value_of("DIR").unwrap_or(".")).types(music_types).build();
-
+    let walker = build_music_walker(args.value_of("DIR").unwrap_or(".")).expect(
+        "Error building music walker",
+    );
     for result in walker {
         match result {
             Ok(entry) => {
