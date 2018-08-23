9fbabd2055675eeea2953ce7137d847619cd4000
Chris Down <chris@chrisdown.name>
2018-08-22 23:24:15 -0400

    Build human-readable feat string

--- src/fixers.rs
+++ src/fixers.rs
@@ -19,11 +19,39 @@ fn make_title(title: &TrackFeat, artist: &TrackFeat) -> String {
     let mut featured_artists = title.featured_artists.clone();
     featured_artists.extend(artist.featured_artists.clone());
 
-    let new_title = format!("{} (feat. {:?})", title.title, featured_artists);
+    let mut new_title = title.title.clone();
+    if !featured_artists.is_empty() {
+        let feat_artists_string = make_feat_string(featured_artists);
+        let feat_string = format!(" (feat. {})", feat_artists_string);
+        new_title.push_str(&feat_string);
+    }
 
     println!("{}", new_title);
 
     new_title
+}
+
+fn make_feat_string(featured_artists: Vec<String>) -> String {
+    let mut output = "".to_owned();
+    let mut artist_idx = 1;
+
+    let mut artists = featured_artists.iter().peekable();
+
+    while let Some(artist) = artists.next() {
+        let is_last = artists.peek().is_none();
+
+        if is_last && artist_idx > 2 {
+            output.push_str(", and ");
+        } else if is_last && artist_idx == 2 {
+            output.push_str(" and ");
+        } else if artist_idx != 1 {
+            output.push_str(", ");
+        }
+        output.push_str(artist);
+        artist_idx += 1;
+    }
+
+    output
 }
 
 fn fixer_is_blacklisted(tags: &taglib::Tag) -> Result<(), MackError> {
