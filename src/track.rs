65ff472935cca1b6c1eb914f6586ad6469a7d101
Chris Down <chris@chrisdown.name>
2018-08-30 21:13:46 +0100

    Don't construct &str from track path
    As of
    https://github.com/ebassi/taglib-rust/commit/4ac35595cf338b0e71fe8bec9a365bc54c3b2cf5,
    this isn't needed.

--- src/track.rs
+++ src/track.rs
@@ -3,6 +3,6 @@ use taglib::File as TFile;
 use types::{MackError, Track};
 
 pub fn get_track(path: PathBuf) -> Result<Track, MackError> {
-    let tag_file = TFile::new(path.clone().to_str().ok_or(MackError::InvalidUnicode)?)?;
+    let tag_file = TFile::new(&path)?;
     Ok(Track { path, tag_file })
 }
