60e6b869a92e795dcd7abbb5f86cb9f445edf36f
Chris Down <chris@chrisdown.name>
2018-08-30 21:09:09 +0100

    Move mtime checks to mtime file

---
+++ src/mtime.rs
@@ -0,0 +1,42 @@
+use std::fs;
+use std::path::{Path, PathBuf};
+use std::time::SystemTime;
+use types;
+
+const LASTMACK_NAME: &str = ".lastmack";
+
+fn make_lastmack_path(base_path: &PathBuf) -> PathBuf {
+    let mut last_run_path = base_path.clone();
+    last_run_path.push(LASTMACK_NAME);
+    last_run_path
+}
+
+pub fn get_last_run_time(base_path: &PathBuf) -> Option<SystemTime> {
+    let last_run_path = make_lastmack_path(&base_path);
+    match get_mtime(&last_run_path) {
+        Ok(mtime) => Some(mtime),
+        Err(err) => {
+            eprintln!(
+                "can't get time from {}, checking all: {:?}",
+                last_run_path.display(),
+                err
+            );
+            None
+        }
+    }
+}
+
+fn get_mtime<T: AsRef<Path>>(path: T) -> Result<SystemTime, types::MackError> {
+    let stat = fs::metadata(path.as_ref())?;
+    Ok(stat.modified()?)
+}
+
+pub fn set_last_run_time(base_path: &PathBuf) -> Result<(), types::MackError> {
+    let last_run_path = make_lastmack_path(&base_path);
+    fs::File::create(&last_run_path)?;
+    Ok(())
+}
+
+pub fn mtime_def_now<T: AsRef<Path>>(path: T) -> SystemTime {
+    get_mtime(path.as_ref()).unwrap_or_else(|_| SystemTime::now())
+}
