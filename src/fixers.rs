74cfa6ec65205534d19731a7238dcc4221ad9602
Chris Down <chris@chrisdown.name>
2019-10-25 14:30:03 +0100

    mack: Replace Unicode ellipsis with "..."

--- src/fixers.rs
+++ src/fixers.rs
@@ -80,7 +80,7 @@ fn make_title(title: &TrackFeat, artist: &TrackFeat) -> String {
     }
 
     new_title = new_title.trim().to_owned();
-    new_title.replace("[", "(").replace("]", ")")
+    new_title.replace("[", "(").replace("]", ")").replace("…", "...")
 }
 
 fn make_feat_string(featured_artists: &[String]) -> String {
