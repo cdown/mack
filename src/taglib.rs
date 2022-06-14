9335eb63e6e823c263d91d8f0b93affc64c90f77
Lewis Belcher <belcher_lewis@protonmail.com>
2022-06-14 11:46:59 +0200

    Introduce id3 crate
    NB this is still WIP.

--- src/taglib.rs
+++ src/taglib.rs
@@ -1,5 +1,7 @@
 use std::path::PathBuf;
 
+use id3::{self, TagLike};
+
 /// A representation of an audio file, with meta-data and properties.
 pub struct File {
 	path: PathBuf,
@@ -8,16 +10,16 @@ pub struct File {
 impl File {
 	/// Creates a new `taglib::File` for the given `path`.
 	pub fn new(path: &PathBuf) -> Result<File, FileError> {
-		Ok(File {
-			path: path.clone()
-		})
+		Ok(File { path: path.clone() })
 	}
 
 	/// Returns the `taglib::Tag` instance for the given file.
 	pub fn tag(&self) -> Result<Tag, FileError> {
-		Ok(Tag {
-			path: &self.path,
-		})
+		if let Ok(tag) = id3::Tag::read_from_path(&self.path) {
+			Ok(Tag { tag })
+		} else {
+			Err(FileError::NoAvailableTag)
+		}
 	}
 
 	/// Updates the meta-data of the file.
@@ -43,49 +45,57 @@ pub enum FileError {
 /// Each `Tag` instance can only be created by the `taglib::File::tag()`
 /// method.
 #[allow(dead_code)]
-pub struct Tag<'a> {
-	path: &'a PathBuf,
+pub struct Tag {
+	tag: id3::Tag,
 }
 
-impl Tag<'_> {
+impl Tag {
 	/// Returns the track name, or an empty string if no track name is present.
 	pub fn title(&self) -> Option<String> {
-		Some(format!("Title of '{:?}'", self.path))
+		self.tag.title().and_then(|x| Some(x.to_string()))
 	}
 
 	/// Sets the track name.
-	pub fn set_title(&mut self, title: &str) {}
+	pub fn set_title(&mut self, title: &str) {
+		self.tag.set_title(title)
+	}
 
 	/// Returns the artist name, or an empty string if no artist name is present.
 	pub fn artist(&self) -> Option<String> {
-		Some(format!("Artist of '{:?}'", self.path))
+		self.tag.artist().and_then(|x| Some(x.to_string()))
 	}
 
 	/// Sets the artist name.
-	pub fn set_artist(&mut self, artist: &str) {}
+	pub fn set_artist(&mut self, artist: &str) {
+		self.tag.set_artist(artist)
+	}
 
 	/// Returns the album name, or an empty string if no album name is present.
 	pub fn album(&self) -> Option<String> {
-		Some(format!("Album of '{:?}'", self.path))
+		self.tag.album().and_then(|x| Some(x.to_string()))
 	}
 
 	/// Sets the album name.
-	pub fn set_album(&mut self, album: &str) {}
+	pub fn set_album(&mut self, album: &str) {
+		self.tag.set_album(album)
+	}
 
 	/// Returns the track comment, or an empty string if no track comment is
 	/// present.
 	pub fn comment(&self) -> Option<String> {
-		Some(format!("Comment of '{:?}'", self.path))
+		None // TODO: Look into joining the comments iterator
 	}
 
 	/// Sets the track comment.
-	pub fn set_comment(&mut self, comment: &str) {}
+	pub fn set_comment(&mut self, comment: &str) {} // TODO
 
 	/// Returns the track number, or 0 if no track number is present.
 	pub fn track(&self) -> Option<u32> {
-		Some(2)
+		self.tag.track()
 	}
 
 	/// Sets the track number.
-	pub fn set_track(&mut self, track: u32) {}
+	pub fn set_track(&mut self, track: u32) {
+		self.tag.set_track(track)
+	}
 }
