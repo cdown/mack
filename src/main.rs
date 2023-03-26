bb455ba985b60fefc45ff2e1d2c95b02fd3cc697
Chris Down <chris@chrisdown.name>
2023-03-26 15:04:53 -0700

    clippy: Use Result::ok instead of e.ok() closure

--- src/main.rs
+++ src/main.rs
@@ -111,7 +111,7 @@ fn fix_all_tracks(base_path: &PathBuf, output_path: &Path, dry_run: bool, force:
 
     let walker = WalkDir::new(base_path)
         .into_iter()
-        .filter_map(|e| e.ok())
+        .filter_map(std::result::Result::ok)
         .map(|e| e.path().to_path_buf())
         .filter(|e| ALLOWED_EXTS.contains(&e.extension().unwrap_or_else(|| OsStr::new(""))))
         .filter(|e| is_eligible_for_fixing(e, last_run_time, force));
