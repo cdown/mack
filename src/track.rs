737ce3aa14938e116a5e77e8d23a2fa451d351d0
Ben V. Brown <Ralim@Ralimtek.com>
2023-07-20 11:22:04 +1000

    Draft pass conversion

--- src/track.rs
+++ src/track.rs
@@ -1,9 +1,9 @@
 use crate::types::Track;
 use anyhow::Result;
-use id3::Tag;
+use audiotags::Tag;
 use std::path::PathBuf;
 
 pub fn get_track(path: PathBuf) -> Result<Track> {
-    let tag = Tag::read_from_path(&path)?;
+    let tag = Tag::new().read_from_path(&path)?;
     Ok(Track { path, tag })
 }
