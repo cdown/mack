bc3e9b7f0ae25e7504f9ac8fc28a1e7ba3c99467
Chris Down <chris@chrisdown.name>
2025-02-14 15:54:50 +0000

    extract: Use if let

--- src/extract.rs
+++ src/extract.rs
@@ -15,48 +15,43 @@ static FEAT_ARTIST_SPLIT: Lazy<Regex> =
     Lazy::new(|| Regex::new(r#", (?:and |& )?"#).expect("BUG: Invalid regex"));
 
 pub fn extract_feat(title: &str) -> TrackFeat {
-    let caps = FEAT_RE.captures(title);
+    if let Some(caps) = FEAT_RE.captures(title) {
+        let trimmed = caps["feat_artists"].trim();
+        let mut feat_artists: Vec<String> = FEAT_ARTIST_SPLIT
+            .split(trimmed)
+            .map(ToOwned::to_owned)
+            .collect();
+        let last_artist = feat_artists
+            .last()
+            .expect("BUG: captured, but no featured artists");
 
-    match caps {
-        Some(caps) => {
-            let trimmed = caps["feat_artists"].trim();
-            let mut feat_artists: Vec<String> = FEAT_ARTIST_SPLIT
-                .split(trimmed)
-                .map(std::borrow::ToOwned::to_owned)
-                .collect();
-            let last_artist = feat_artists
-                .last()
-                .expect("BUG: captured, but no featured artists");
-
-            // If the last artist contains an "&", we'll split on it, even without a comma. This
-            // isn't perfect, but is mostly right.
-            for amp_split in AMP_SPLITS {
-                if last_artist.contains(amp_split) {
-                    let mut tmp_last_split: Vec<String> = last_artist
-                        .rsplitn(2, amp_split)
-                        .map(std::borrow::ToOwned::to_owned)
-                        .collect();
-                    tmp_last_split.reverse();
-                    feat_artists.pop();
-                    feat_artists.append(&mut tmp_last_split);
-                    break;
-                }
-            }
-
-            let featless_title = FEAT_RE.replace_all(title, "").trim().to_owned();
-            TrackFeat {
-                title: featless_title,
-                featured_artists: feat_artists,
-                original_title: title.to_string(),
+        // If the last artist contains an "&", we'll split on it, even without a comma. This
+        // isn't perfect, but is mostly right.
+        for amp_split in AMP_SPLITS {
+            if last_artist.contains(amp_split) {
+                let mut tmp_last_split: Vec<String> = last_artist
+                    .rsplitn(2, amp_split)
+                    .map(ToOwned::to_owned)
+                    .collect();
+                tmp_last_split.reverse();
+                feat_artists.pop();
+                feat_artists.append(&mut tmp_last_split);
+                break;
             }
         }
-        None => {
-            // There's no "feat" in here, just return the title whole
-            TrackFeat {
-                title: title.to_string(),
-                featured_artists: Vec::new(),
-                original_title: title.to_string(),
-            }
+
+        let featless_title = FEAT_RE.replace_all(title, "").trim().to_owned();
+        TrackFeat {
+            title: featless_title,
+            featured_artists: feat_artists,
+            original_title: title.to_string(),
+        }
+    } else {
+        // There's no "feat" in here, just return the title whole
+        TrackFeat {
+            title: title.to_string(),
+            featured_artists: Vec::new(),
+            original_title: title.to_string(),
         }
     }
 }
